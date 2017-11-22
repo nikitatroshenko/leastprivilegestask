a1e9e0f8dfcd5388e276120c52cc185305084b1c
nikitatroshenko <nikitatroshenko@gmail.com>
2017-11-22 18:31:40 +0300

    Restructurized project
    Added interface stubs, split project files into include/ and src/ directories

---
+++ Atroshenko/src/interface/main.c
@@ -0,0 +1,13 @@
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
